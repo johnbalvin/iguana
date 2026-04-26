24e41dbb06df3265ff309ed0f107284f9ed9bdcc
John <johnchristian@hotmail.es>
2026-04-26 00:08:54 -0500

    improve regex query

--- files/variables.go
+++ files/variables.go
@@ -2,10 +2,10 @@ package files
 
 import "regexp"
 
-//['"][\w\.\-_/ ]+?((\.js)|(\.json)|(\.woff)|(\.woff2)|(\.ttf)|(\.mjs)|(\.txt)|(\.css)|(\.md)|(\.gif)|(\.ico)|(\.jpeg)|(\.jpg)|(\.png)|(\.tiff)|(\.tif)|(\.svg))['"]`) //matchear todos las url de js,css,mjs y svg para que sean reemplazados
-//regJs: regex to find .js files,regServiceW: regex to find service workers definitions
-//regStatic: regex to find static files urls, files ending in .js,.json,.woff,.woff2,.ttf,.json,.mjs,.txt,.css,.md,.gif,.ico,.jpeg,.jpg, .png, .tiff, .tif, .svg
-var RegStatic = regexp.MustCompile(`['"][\w\.\-_/ ]+?((\.js)|(\.json)|(\.woff)|(\.woff2)|(\.ttf)|(\.json)|(\.mjs)|(\.txt)|(\.css)|(\.md)|(\.gif)|(\.ico)|(\.jpeg)|(\.jpg)|(\.png)|(\.tiff)|(\.tif)|(\.svg))['"]`) //matchear todos las url de js,css,mjs y svg para que sean reemplazados
+// ['"][\w\.\-_/ ]+?((\.js)|(\.json)|(\.woff)|(\.woff2)|(\.ttf)|(\.mjs)|(\.txt)|(\.css)|(\.md)|(\.gif)|(\.ico)|(\.jpeg)|(\.jpg)|(\.png)|(\.tiff)|(\.tif)|(\.svg))['"]`) //matchear todos las url de js,css,mjs y svg para que sean reemplazados
+// regJs: regex to find .js files,regServiceW: regex to find service workers definitions
+// regStatic: regex to find static files urls, files ending in .js,.json,.woff,.woff2,.ttf,.json,.mjs,.txt,.css,.md,.gif,.ico,.jpeg,.jpg, .png, .tiff, .tif, .svg
+var RegStatic = regexp.MustCompile(`['"][\w.\-/ ]+?(\.(?:js|json|woff2?|ttf|mjs|txt|css|md|gif|ico|jpe?g|png|tiff?|svg))['"]`) //matchear todos las url de js,css,mjs y svg para que sean reemplazados
 var RegServiceW = regexp.MustCompile(`(serviceWorker\.register\()['"][\w\-\._/]+?(\.js)['"]`)
 var RegJs = regexp.MustCompile(`['"][\w\.\-_/]+?(\.js)['"]`)
 var RegURL = regexp.MustCompile("[^a-zA-Z0-9]+")
